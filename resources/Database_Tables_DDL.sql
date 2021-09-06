cc6d9a4a5396bb5c04431d7ea9a418dcafe26cc7
rohitrichhariya <90218564+rohitrichhariya@users.noreply.github.com>
2021-09-07 02:04:50 +0530

    Add files via upload
    add comic table DDL

--- resources/Database_Tables_DDL.sql
+++ resources/Database_Tables_DDL.sql
@@ -1,1 +1,12 @@
-sql file
+CREATE TABLE `comic` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`comic_number` VARCHAR(500) NOT NULL,
+	`comic_name` VARCHAR(500) NOT NULL,
+	`comic_alt` TEXT NULL,
+	`comic_link` TEXT NULL,
+	`comic_image_link` TEXT NULL,
+	`comic_image` LONGBLOB NULL,
+	PRIMARY KEY (`id`)
+)
+COMMENT='This table will store the details of all comics which are requested using xkcd API';
+
