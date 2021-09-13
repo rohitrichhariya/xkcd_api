1eef44410db01fd78b0dd202298f9001eb9c67b1
rohitrichhariya <90218564+rohitrichhariya@users.noreply.github.com>
2021-09-13 13:00:25 +0530

    Add files via upload
    add init

--- testing/__init__.py
+++ testing/__init__.py
@@ -1,1 +1,20 @@
+import requests
 
+jpg_image = requests.get("https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg")
+
+test_comic_record = {
+    'num': 1,
+    'safe_title': 'Barrel - Part 1',
+    'alt': 'Don''t we all',
+    'img': 'https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg',
+    'comic_url': 'https://xkcd.com/1/info.0.json'
+}
+
+test_comic_url = 'https://xkcd.com/1/info.0.json'
+test_image_url = 'https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg'
+
+comic_id = 1
+
+test_image = None
+with open('barrel_cropped_(1).jpg', 'rb') as xkcd_response_1:
+    test_image = xkcd_response_1.read()
